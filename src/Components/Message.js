4aa648ce96fc38bc68720adcaaf12e65408eede6
HuynhQuocTien <quoctien01062003@gmail.com>
2025-03-07 00:20:34 +0700

    Remove unused assets and components; set up Vite configuration for React

--- src/Components/Message.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Message({text,sent}) {
-  return (
-    <div className={`message ${sent ? 'sent' : 'received'}`}>
-        <div className='message-bubble'>{text}</div>
-    </div>
-  )
-}
